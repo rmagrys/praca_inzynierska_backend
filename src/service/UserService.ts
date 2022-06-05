c4fcbc901afaa32eb7e964395da24828a4b0a7c6
Rafal Magrys <rafal.alpha@expertido.com>
2022-06-06 01:11:31 +0200

    added services

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,4 +1,6 @@
+import { NotFoundError } from "routing-controllers";
 import { Service } from "typedi";
+import { DeleteResult } from "typeorm";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { User } from "../entity/User";
 import { UserRepository } from "../repository/UserRepository";
@@ -10,14 +12,38 @@ export class UserService {
   ) {}
 
   async getAllUsers(): Promise<User[]> {
-    return await this.userRepository.find();
+    return this.userRepository.find();
   }
 
   async findOneUser(id: string): Promise<User> {
-    return await this.userRepository.findOne(id);
+    return this.userRepository.findOne(id);
   }
 
   async saveUser(user: User): Promise<User> {
-    return await this.userRepository.save(user);
+    return this.userRepository.save(user);
+  }
+
+  async updateUser(id: string, user: User): Promise<User> {
+    return this.userRepository
+      .findOne(id)
+      .then((userToUpdate) => {
+        userToUpdate.email = user.email;
+        userToUpdate.firstName = user.firstName;
+        userToUpdate.lastName = user.lastName;
+
+        return this.userRepository.save(userToUpdate);
+      })
+      .catch(() => {
+        throw new NotFoundError(`User with id ${id} not found`);
+      });
+  }
+
+  async deleteUser(id: string): Promise<DeleteResult> {
+    return <DeleteResult> await this.userRepository
+      .findOne(id)
+      .then((userToDelete) => this.userRepository.delete(userToDelete.id))
+      .catch(() => {
+        throw new NotFoundError(`User with id ${id} not found`);
+      });
   }
 }
